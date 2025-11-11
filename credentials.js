5ba4cb315bb1540c7ea5f99adde8accc1fc06b08
eugene1hk <liue3@kgv.hk>
2025-11-11 10:49:11 +0800

    Add files via upload

---
+++ credentials.js
@@ -0,0 +1,6 @@
+// credentials.js — private local version (DO NOT upload!)
+window.credentials = {
+  usernames: ["AdminUser", "123"],
+  callsigns: ["LRC-001", "202"],
+  passwords: ["mysecurepass", "202"]
+};
